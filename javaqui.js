cd2698ae1698e1093962695110bcdc6f7c196273
sunual <69469348+sunual@users.noreply.github.com>
2021-10-16 17:46:05 -0300

    Update javaqui.js

--- javaqui.js
+++ javaqui.js
@@ -3,7 +3,7 @@ const topi = new Array();
 sub.forEach(sub => {
   topi.push(Element.textContent);
 });
-junio = document.querySelector('ul');
+junio = document.querySelector('ol');
 topi.forEach(topi => {
 const item = document.createElement('li');
 item.textContent = topi;
