02284f401d09e5db69e237c74ca1d0e133677c5a
sunual <69469348+sunual@users.noreply.github.com>
2021-10-16 17:23:44 -0300

    Create javaqui.js

---
+++ javaqui.js
@@ -0,0 +1,7 @@
+const sub = document.querySelectorAll('h2');
+const top = new Array();
+sub.forEach(sub => {
+  top.push(Element.textContent);
+});
+junio = document.querySelector('ul');
+junior.textContent = top;
